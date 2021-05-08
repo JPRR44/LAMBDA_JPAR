35f6b8976dd81ab1b7180e2c6b546d6f103591eb
BuildTools <is713932@iteso.mx>
2021-05-08 17:43:08 -0500

    Feat: Create getTicketById lambda

---
+++ getTicketById.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'JPAR'
+
+exports.handler = async (event,context,callback) => {
+    const params = {
+        TableName: tableName
+    };
+    const data = await documentClient.scan(params).promise();
+    const dataFiltered = data.Items.filter(ticket => ticket.id === event.id)
+    if(!data){
+        throw Error(`There was an error fetching the data from ${tableName}`);
+    }
+    return dataFiltered
+};
