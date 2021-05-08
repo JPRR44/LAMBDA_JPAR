13b54515b381404386c61c12738eecd8fbe14cbf
BuildTools <is713932@iteso.mx>
2021-05-08 02:39:36 -0500

    Feat: Update a ticket given a valid ticket json schema
    The only parameters that are allowed to be updated are: assignedDev2,assignedDev3,email,attachedFiles,description.
    
    The passed ticket must have a valid id and summary

---
+++ requests/updateTicket.js
@@ -0,0 +1,121 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'JPAR'
+
+exports.handler = async (event, context, callback) => {
+
+    let response = {}
+
+    if (event.ticket.assignedDev2 != '') {
+        var params = {
+            TableName: tableName,
+            Item: event.ticket,
+            Key: {
+                "id": event.ticket.id, //primary key and primary key value
+                "summary": event.ticket.summary
+            },
+            UpdateExpression: "set assignedDev2 = :n",
+            ExpressionAttributeValues: {
+                ":n": event.ticket.assignedDev2
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        const data = await documentClient.update(params).promise();
+        if (!data) {
+            throw Error("There was an error fetching the data from table");
+        }
+        //add the property 'assignedDev2' to the response object
+        response['assignedDev2'] = data.Attributes.assignedDev2
+    }
+    if (event.ticket.assignedDev3 != '') {
+        var params = {
+            TableName: tableName,
+            Item: event.ticket,
+            Key: {
+                "id": event.ticket.id, //primary key and primary key value
+                "summary": event.ticket.summary
+            },
+            UpdateExpression: "set assignedDev3 = :n",
+            ExpressionAttributeValues: {
+                ":n": event.ticket.assignedDev3
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        const data = await documentClient.update(params).promise();
+        if (!data) {
+            throw Error("There was an error fetching the data from table");
+        }
+        //add the property 'assignedDev3' to the response object
+        response['assignedDev3'] = data.Attributes.assignedDev3
+    }
+    if (event.ticket.attachedFiles != '') {
+        var params = {
+            TableName: tableName,
+            Item: event.ticket,
+            Key: {
+                "id": event.ticket.id, //primary key and primary key value
+                "summary": event.ticket.summary
+            },
+            UpdateExpression: "set attachedFiles = :n",
+            ExpressionAttributeValues: {
+                ":n": event.ticket.attachedFiles
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        const data = await documentClient.update(params).promise();
+        if (!data) {
+            throw Error("There was an error fetching the data from table");
+        }
+        //add the property 'attachedFiles' to the response object
+        response['attachedFiles'] = data.Attributes.attachedFiles
+    }
+    if (event.ticket.description != '') {
+        var params = {
+            TableName: tableName,
+            Item: event.ticket,
+            Key: {
+                "id": event.ticket.id, //primary key and primary key value
+                "summary": event.ticket.summary
+            },
+            UpdateExpression: "set description = :n",
+            ExpressionAttributeValues: {
+                ":n": event.ticket.description
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        const data = await documentClient.update(params).promise();
+        if (!data) {
+            throw Error("There was an error fetching the data from table");
+        }
+        //add the property 'description' to the response object
+        response['description'] = data.Attributes.description
+    }
+    if (event.ticket.email != '') {
+        var params = {
+            TableName: tableName,
+            Item: event.ticket,
+            Key: {
+                "id": event.ticket.id, //primary key and primary key value
+                "summary": event.ticket.summary
+            },
+            UpdateExpression: "set email = :n",
+            ExpressionAttributeValues: {
+                ":n": event.ticket.email
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        const data = await documentClient.update(params).promise();
+        if (!data) {
+            throw Error("There was an error fetching the data from table");
+        }
+        //add the property 'email' to the response object
+        response['email'] = data.Attributes.email
+    }
+
+    return response
+};
