e04c2cba16dbe122a899500e46e05829dc58a20f
JPRR44 <juanp.robles@hotmail.com>
2021-05-07 21:35:58 -0500

    Feat: Added get filtedes tickets by email functionality

---
+++ requests/getOneTicket.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'JPAR'
+
+exports.handler = async (event,context,callback) => {
+    const params = {
+        TableName: tableName
+    };
+    const data = await documentClient.scan(params).promise();
+    const dataFiltered = data.Items.filter(ticket => ticket.email === event.L3email)
+    if(!data){
+        throw Error(`There was an error fetching the data from ${TableName}`);
+    }
+    return dataFiltered
+};
