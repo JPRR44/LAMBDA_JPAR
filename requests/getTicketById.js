f1f9144fe0e1fd13764e521012dfa2ba7ec9cbe0
BuildTools <is713932@iteso.mx>
2021-05-09 14:47:48 -0500

    Feat: Verify it the ticket exists, and return error 400 otherwise

--- requests/getTicketById.js
+++ requests/getTicketById.js
@@ -11,5 +11,10 @@ exports.handler = async (event,context,callback) => {
     if(!data){
         throw Error(`There was an error fetching the data from ${tableName}`);
     }
-    return dataFiltered
+    if(dataFiltered.length > 0){
+        return dataFiltered
+    }else {
+        callback('[400] The provided ticket id does not exist')
+        return {}
+    }
 };
