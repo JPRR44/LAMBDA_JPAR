a16f54d632d73f153395e756d17268afb3ff5092
BuildTools <is713932@iteso.mx>
2021-05-09 14:22:46 -0500

    Feat: Check if the ticket id exists before updating values
    Return status code 400 if there isnt a ticket with that id

--- requests/updateTicket.js
+++ requests/updateTicket.js
@@ -6,111 +6,124 @@ exports.handler = async (event, context, callback) => {
 
     let response = {}
 
-    if (event.ticket.assignedDev2 != '') {
-        var params = {
-            TableName: tableName,
-            Item: event.ticket,
-            Key: {
-                "id": event.ticket.id //primary key and primary key value
-            },
-            UpdateExpression: "set assignedDev2 = :n",
-            ExpressionAttributeValues: {
-                ":n": event.ticket.assignedDev2
-            },
-            ReturnValues: "UPDATED_NEW"
-        };
+    //check if the a ticket with the passed id exists
+    const searchParams = {
+        TableName: tableName
+    };
+    const data = await documentClient.scan(searchParams).promise()
+    const dataFiltered = data.Items.filter(ticket => ticket.id === event.ticket.id)
+    if (dataFiltered.length > 0) {
+        //update the required fields
 
-        const data = await documentClient.update(params).promise();
-        if (!data) {
-            throw Error("There was an error fetching the data from table");
+        if (event.ticket.assignedDev2 != '') {
+            var params = {
+                TableName: tableName,
+                Item: event.ticket,
+                Key: {
+                    "id": event.ticket.id //primary key and primary key value
+                },
+                UpdateExpression: "set assignedDev2 = :n",
+                ExpressionAttributeValues: {
+                    ":n": event.ticket.assignedDev2
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
+
+            const data = await documentClient.update(params).promise();
+            if (!data) {
+                throw Error("There was an error fetching the data from table");
+            }
+            //add the property 'assignedDev2' to the response object
+            response['assignedDev2'] = data.Attributes.assignedDev2
         }
-        //add the property 'assignedDev2' to the response object
-        response['assignedDev2'] = data.Attributes.assignedDev2
-    }
-    if (event.ticket.assignedDev3 != '') {
-        var params = {
-            TableName: tableName,
-            Item: event.ticket,
-            Key: {
-                "id": event.ticket.id //primary key and primary key value
-            },
-            UpdateExpression: "set assignedDev3 = :n",
-            ExpressionAttributeValues: {
-                ":n": event.ticket.assignedDev3
-            },
-            ReturnValues: "UPDATED_NEW"
-        };
+        if (event.ticket.assignedDev3 != '') {
+            var params = {
+                TableName: tableName,
+                Item: event.ticket,
+                Key: {
+                    "id": event.ticket.id //primary key and primary key value
+                },
+                UpdateExpression: "set assignedDev3 = :n",
+                ExpressionAttributeValues: {
+                    ":n": event.ticket.assignedDev3
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
 
-        const data = await documentClient.update(params).promise();
-        if (!data) {
-            throw Error("There was an error fetching the data from table");
+            const data = await documentClient.update(params).promise();
+            if (!data) {
+                throw Error("There was an error fetching the data from table");
+            }
+            //add the property 'assignedDev3' to the response object
+            response['assignedDev3'] = data.Attributes.assignedDev3
         }
-        //add the property 'assignedDev3' to the response object
-        response['assignedDev3'] = data.Attributes.assignedDev3
-    }
-    if (event.ticket.attachedFiles != '') {
-        var params = {
-            TableName: tableName,
-            Item: event.ticket,
-            Key: {
-                "id": event.ticket.id //primary key and primary key value
-            },
-            UpdateExpression: "set attachedFiles = :n",
-            ExpressionAttributeValues: {
-                ":n": event.ticket.attachedFiles
-            },
-            ReturnValues: "UPDATED_NEW"
-        };
+        if (event.ticket.attachedFiles != '') {
+            var params = {
+                TableName: tableName,
+                Item: event.ticket,
+                Key: {
+                    "id": event.ticket.id //primary key and primary key value
+                },
+                UpdateExpression: "set attachedFiles = :n",
+                ExpressionAttributeValues: {
+                    ":n": event.ticket.attachedFiles
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
 
-        const data = await documentClient.update(params).promise();
-        if (!data) {
-            throw Error("There was an error fetching the data from table");
+            const data = await documentClient.update(params).promise();
+            if (!data) {
+                throw Error("There was an error fetching the data from table");
+            }
+            //add the property 'attachedFiles' to the response object
+            response['attachedFiles'] = data.Attributes.attachedFiles
         }
-        //add the property 'attachedFiles' to the response object
-        response['attachedFiles'] = data.Attributes.attachedFiles
-    }
-    if (event.ticket.description != '') {
-        var params = {
-            TableName: tableName,
-            Item: event.ticket,
-            Key: {
-                "id": event.ticket.id //primary key and primary key value
-            },
-            UpdateExpression: "set description = :n",
-            ExpressionAttributeValues: {
-                ":n": event.ticket.description
-            },
-            ReturnValues: "UPDATED_NEW"
-        };
+        if (event.ticket.description != '') {
+            var params = {
+                TableName: tableName,
+                Item: event.ticket,
+                Key: {
+                    "id": event.ticket.id //primary key and primary key value
+                },
+                UpdateExpression: "set description = :n",
+                ExpressionAttributeValues: {
+                    ":n": event.ticket.description
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
 
-        const data = await documentClient.update(params).promise();
-        if (!data) {
-            throw Error("There was an error fetching the data from table");
+            const data = await documentClient.update(params).promise();
+            if (!data) {
+                throw Error("There was an error fetching the data from table");
+            }
+            //add the property 'description' to the response object
+            response['description'] = data.Attributes.description
         }
-        //add the property 'description' to the response object
-        response['description'] = data.Attributes.description
-    }
-    if (event.ticket.email != '') {
-        var params = {
-            TableName: tableName,
-            Item: event.ticket,
-            Key: {
-                "id": event.ticket.id //primary key and primary key value
-            },
-            UpdateExpression: "set email = :n",
-            ExpressionAttributeValues: {
-                ":n": event.ticket.email
-            },
-            ReturnValues: "UPDATED_NEW"
-        };
+        if (event.ticket.email != '') {
+            var params = {
+                TableName: tableName,
+                Item: event.ticket,
+                Key: {
+                    "id": event.ticket.id //primary key and primary key value
+                },
+                UpdateExpression: "set email = :n",
+                ExpressionAttributeValues: {
+                    ":n": event.ticket.email
+                },
+                ReturnValues: "UPDATED_NEW"
+            };
 
-        const data = await documentClient.update(params).promise();
-        if (!data) {
-            throw Error("There was an error fetching the data from table");
+            const data = await documentClient.update(params).promise();
+            if (!data) {
+                throw Error("There was an error fetching the data from table");
+            }
+            //add the property 'email' to the response object
+            response['email'] = data.Attributes.email
         }
-        //add the property 'email' to the response object
-        response['email'] = data.Attributes.email
-    }
 
-    return response
+        return response
+    }else{
+        callback('[400] The provided ticket id does not exist')
+        return {}
+    }
 };
